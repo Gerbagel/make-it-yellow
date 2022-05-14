a14615c77120d69fffbaf4147684b5a5404559fa
Gerbagel <33951616+Gerbagel@users.noreply.github.com>
2022-05-15 01:48:25 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Make mixed yellow
+// @namespace    https://gerbagel.github.io/
+// @version      1.0
+// @description  make mixed canon/filler episodes on animefillerlist.com yellow.
+// @author       Gerbagel
+// @match        https://www.animefillerlist.com/shows/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=animefillerlist.com
+// @copyright    2022+, Gerbagel
+// @grant        none
+// ==/UserScript==
+
+(() => {
+    'use strict';
+
+    var thing = document.querySelectorAll(".Type span");
+    for (var i = 0; i < thing.length; i++) {
+        if (thing[i].textContent === "Mixed Canon/Filler") {
+            thing[i].style.background = "#ffed33";
+            thing[i].style.color = "#696969";   // nice.
+        }
+    }
+})();
